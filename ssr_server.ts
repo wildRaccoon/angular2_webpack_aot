7f919c729ac08f23083ccee6579014569e456ccc
wildRaccoon <chukvm@gmail.com>
2018-08-21 12:25:15 +0300

    fix ssr

--- ssr_server.ts
+++ ssr_server.ts
@@ -17,7 +17,8 @@ const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist');
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/dev_ssr/app');
+const { AppServerModule, LAZY_MODULE_MAP } = require('./dist/dev_ssr/app');
+console.log(AppServerModule);
 
 // Express Engine
 import { ngExpressEngine } from '@nguniversal/express-engine';
@@ -25,7 +26,7 @@ import { ngExpressEngine } from '@nguniversal/express-engine';
 import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
 app.engine('html', ngExpressEngine({
-  bootstrap: AppServerModuleNgFactory,
+  bootstrap: AppServerModule,
   providers: [
     provideModuleMap(LAZY_MODULE_MAP)
   ]
