5819fd304f05572f4363e41d762f2c9a2b624afc
wildRaccoon <chukvm@gmail.com>
2019-05-30 22:25:41 +0300

    update deps

--- config/loaders/sass-header-loader.ts
+++ config/loaders/sass-header-loader.ts
@@ -3,7 +3,7 @@ var header:string = path.join(process.cwd(),'src/assets/css/theme.scss');
 var fs = require('fs');
  
 function loader(text:any):any {
-    //console.log("\n" + this.resourcePath + "\n");
+    console.log("\n" + this.resourcePath + "\n");
     
 
     var contents = fs.readFileSync(header, 'utf8');
